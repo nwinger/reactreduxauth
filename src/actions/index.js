ab3487d75a29306b98f7aa8105eb2f31800a8ec3
Nicklas Winger <nwi@gecko.no>
2016-12-13 20:30:36 +0100

    Add shell for signinUser action creator

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+
+
+export function signInUser({ email, password }) {
+    // Submit email/password to the server
+
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - redirect to the route '/feature'
+
+    // If request is bad...
+    // - Show an error to the user
+    
+};
