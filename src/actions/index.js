1ce667d6f79a66978ac73a926d0eaa5822a3958c
Nicklas Winger <nwi@gecko.no>
2016-12-19 08:13:52 +0100

    Automatically sign in user if token exists and add token as header on protected route on server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,60 +1,74 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signInUser({ email, password }) {
-    return function (dispatch) {
-        // Submit email/password to the server
-        axios.post(`${ROOT_URL}/signin`, { email, password })
-            .then(response => {
-                // If request is good...
-                // - Update state to indicate user is authenticated
-                dispatch({ type: AUTH_USER });
+	return function (dispatch) {
+		// Submit email/password to the server
+		axios.post(`${ROOT_URL}/signin`, { email, password })
+			.then(response => {
+				// If request is good...
+				// - Update state to indicate user is authenticated
+				dispatch({ type: AUTH_USER });
 
-                // - Save the JWT token to localstore
-                const {token} = response.data;
-                localStorage.setItem('token', token);
+				// - Save the JWT token to localstore
+				const {token} = response.data;
+				localStorage.setItem('token', token);
 
-                // - redirect to the route '/feature'
-                browserHistory.push('/feature');
-            })
-            .catch(() => {
-                // If request is bad...
-                // - Show an error to the user
-                dispatch(authError('Bad Signin Info'));
-            });
-    };
-};
+				// - redirect to the route '/feature'
+				browserHistory.push('/feature');
+			})
+			.catch(() => {
+				// If request is bad...
+				// - Show an error to the user
+				dispatch(authError('Bad Signin Info'));
+			});
+	};
+}
 
 export function signUpUser({ email, password }) {
-    return function (dispatch) {
-        axios.post(`${ROOT_URL}/signup`, { email, password })
-            .then(response => {
-                dispatch({ type: AUTH_USER });
-                localStorage.setItem('token', response.data.token);
-                browserHistory.push('/feature');
-            })
-            .catch(response => {
-                const { error } = response.response.data;
-                dispatch(authError(error));
-            });
-    };
+	return function (dispatch) {
+		axios.post(`${ROOT_URL}/signup`, { email, password })
+			.then(response => {
+				dispatch({ type: AUTH_USER });
+				localStorage.setItem('token', response.data.token);
+				browserHistory.push('/feature');
+			})
+			.catch(response => {
+				const { error } = response.response.data;
+				dispatch(authError(error));
+			});
+	};
 }
 
 export function authError(error) {
-    return {
-        type: AUTH_ERROR,
-        payload: error
-    };
-};
+	return {
+		type: AUTH_ERROR,
+		payload: error
+	};
+}
 
 export function signOutUser() {
-    localStorage.removeItem('token');
-    return {
-        type: UNAUTH_USER,
-        payload: null
-    };
+	localStorage.removeItem('token');
+	return {
+		type: UNAUTH_USER,
+		payload: null
+	};
+}
+
+export function fetchMessage() {
+	return function (dispatch) {
+		axios.get(ROOT_URL, {
+			headers: { authorization: localStorage.getItem('token') }
+		})
+			.then(response => {
+				dispatch({
+					type: FETCH_MESSAGE,
+					payload: response.data.message
+				});
+			});
+	};
 }
--- src/index.js
+++ src/index.js
@@ -14,19 +14,29 @@ import Welcome from './components/welcome';
 
 import reducers from './reducers';
 import RequireAuth from './components/auth/require_auth';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+// If we have a token, consider the user to be signed in
+if (token) {
+	// we need to update application state
+	store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Welcome} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/signout" component={Signout} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/feature" component={RequireAuth(Feature)} />
-      </Route>
-    </Router>
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={Welcome} />
+				<Route path="/signin" component={Signin} />
+				<Route path="/signout" component={Signout} />
+				<Route path="/signup" component={Signup} />
+				<Route path="/feature" component={RequireAuth(Feature)} />
+			</Route>
+		</Router>
+	</Provider>
+	, document.querySelector('.container'));
